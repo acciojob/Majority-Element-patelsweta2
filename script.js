695910469cb246921de9561b76ed28ceb6d13cf7
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2023-12-28 19:51:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your code here
+
+function findMajorityElement(arr) {
+    
+    const elementCount = {};
+
+    
+    for (let i = 0; i < arr.length; i++) {
+        const currentElement = arr[i];
+
+        
+        if (elementCount[currentElement]) {
+            elementCount[currentElement]++;
+        } else {
+            elementCount[currentElement] = 1;
+        }
+
+        
+        if (elementCount[currentElement] > Math.floor(arr.length / 2)) {
+            return currentElement;
+        }
+    }
+
+    
+    return -1;
+}
+
+
+const inputArray = [2, 1, 2];
+const majorityElement = findMajorityElement(inputArray);
+console.log(majorityElement);
